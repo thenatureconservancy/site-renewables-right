5319566e2e88b64b1fd14ee6dee50aefaea3ec15
Melanie Bruce <melanie.bruce@tnc.org>
2026-03-30 09:30:12 -0400

    update help panel

--- src/components/TheHelp.vue
+++ src/components/TheHelp.vue
@@ -9,13 +9,25 @@ const mapStore = useMapStore()
   <div class="bg-white q-ma-sm" v-for="(item, index1) in mapStore.layers" :key="index1">
     <div v-for="(layer, index2) in item.subheaders" :key="index2">
       <div class="bg-blue-grey-9 text-white text-center">
-       
         <p class="text-body1">{{ layer.title }}</p>
       </div>
-      <q-list class="">
-        <q-item class="q-pl-sm" v-for="(sublayer, index3) in layer.sublayers" :key="index3" :id="sublayer.elid">
+      <q-list
+        class=""
+        v-for="(sublayer, index3) in layer.sublayers"
+        :key="index3"
+        :id="sublayer.elid"
+      >
+        <q-item class="" v-if="sublayer.elid !== 'whoopingCraneSolar'">
           <q-item-section class="">
-            <q-item-label class="text-bold" :style="(mapStore.activeHelpElement == sublayer.elid) ? 'border: 2px solid green; padding: 5px' : ''">{{ sublayer.title }}</q-item-label>
+            <q-item-label
+              class="text-bold"
+              :style="
+                mapStore.activeHelpElement == sublayer.elid
+                  ? 'border: 2px solid green; padding: 5px'
+                  : ''
+              "
+              >{{ sublayer.title }}</q-item-label
+            >
             <q-item-label v-html="sublayer.longDescription"></q-item-label>
           </q-item-section>
 
