51531cba06468b3b69016bd4ea4414ee8b4f7979
Melanie Bruce <melanie.bruce@tnc.org>
2026-08-05 15:46:48 -0400

    add logo

--- src/App.vue
+++ src/App.vue
@@ -77,11 +77,12 @@ const width = ref(window.innerWidth / 2.2)
   <q-layout view="hHh lpR fFf">
     <q-header class="bg-white text-dark text-body1">
       <q-toolbar>
-        <a href="https://www.nature.org/en-us/">
+        <a href="https://www.nature.org/en-us/" target="_blank">
           <img
-            src="./assets/logo.svg"
-            :style="mobile ? 'width: 100px;' : 'width: 100px; padding-top: 5px; margin: 5px'"
-        /></a>
+            src="./assets/tnc_globe.jpg"
+            style="width: 30px;margin-top:3px; "
+          />
+      </a>
         <q-separator vertical spaced inset></q-separator>
         <span :class="mobile ? 'text-body2' : 'q-ml-md text-weight-bold'">
           Site Renewables Right</span
