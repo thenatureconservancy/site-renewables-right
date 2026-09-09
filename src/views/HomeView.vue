226e0d31b42629465e1b1c008a801105578d16de
Melanie Bruce <melanie.bruce@tnc.org>
2026-09-09 11:51:00 -0400

    scroll bar always visible

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -63,7 +63,11 @@ onMounted(() => {
           class="col-sm-7 col-md-6 col-lg-6 q-pa-lg column self-stretch"
           style="min-height: 0"
         >
-          <q-scroll-area style="height: calc(100vh - 80px); margin-right: -20px" class="q-pa-lg">
+          <q-scroll-area
+            style="height: calc(100vh - 80px); margin-right: -20px"
+            class="q-pa-lg"
+            :visible="true"
+          >
             <div id="topdiv">
               <a href="https://www.nature.org/en-us/">
                 <img
